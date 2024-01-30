23d3bf635801e7fb430ac5365ef3d83585627649
dimitri <dimi.mansour03@gmail.com>
2024-01-30 17:14:48 -0500

    client update

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,16 +1,22 @@
 package Client;
 
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.EOFException;
+import java.io.IOException;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
     private static Socket socket;
     // private ServerListener (thread)
+    State state;
+    
     public Client(ServerConfig config) throws Exception {
         socket = new Socket(config.getServerAddress(), config.getServerPort());
         System.out.format("Server.Serveur lancé sur [%s:%d]", config.getServerAddress(), config.getServerPort());
-
+        this.state = State.NOT_CONNECTED;
+        this.stateHandle();
         Thread listenToServerThread = new Thread(() -> {
             try {
                 this.listenToServer();
@@ -47,6 +53,29 @@ public class Client {
 
     public void listenToUser () {
         // TODO
+    }
+
+    public void stateHandle() throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        while(this.state == State.NOT_CONNECTED){
+            System.out.println("Enter Username");
+            String username = scanner.nextLine();
+            System.out.println("Enter Password");
+            String password = scanner.nextLine();
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            out.writeUTF(username +"\n"+password);
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            String awnser = in.readUTF();
+            if(awnser == "accepted"){
+                System.out.println("Succes Conenction");
+                this.state = State.CONNECTED;
+            } else {
+                System.out.println("Erreur dans la saisie du mot de passe");
+            }
+        }
+        DataInputStream in = new DataInputStream(socket.getInputStream());
+        String fifteenmessage = in.readUTF();
+        System.out.println(fifteenmessage);
     }
 
     public void close() throws Exception {
